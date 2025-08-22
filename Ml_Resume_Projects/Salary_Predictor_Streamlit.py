baa2ec392fe8725825f146858c8b8130afcb7372
Faraz Gill <110542630+FarazTheAnalyst@users.noreply.github.com>
2025-08-22 18:15:21 +0500

    Update Salary_Predictor_Streamlit.py

--- Ml_Resume_Projects/Salary_Predictor_Streamlit.py
+++ Ml_Resume_Projects/Salary_Predictor_Streamlit.py
@@ -19,7 +19,7 @@ st.title("💰 Job Salary Predictor")
 st.markdown("Predict the expected salaries based on experience, job title, gender, education level.")
 
 st.sidebar.subheader("Top Importent Features")
-st.sidebar.image("feature_importance.png", caption="Top features")
+st.sidebar.image("Ml_Resume_Projects/feature_importance.png", caption="Top features")
 
 
 #Main form
@@ -112,4 +112,5 @@ st.markdown("""
             Data sourced from kaggle: Predictions are estimates based on historical data.
 
             """)
+
 
